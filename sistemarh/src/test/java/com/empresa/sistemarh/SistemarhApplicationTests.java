f49e0c83d988b9f4b5ca3472ab8d6d8408387b12
joaquimoiio <joaquimpaes01@gmail.com>
2025-07-27 13:36:38 -0300

    Start

---
+++ sistemarh/src/test/java/com/empresa/sistemarh/SistemarhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.sistemarh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemarhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
